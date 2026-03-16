061f6c6aed3d118663abab3d76898bf5880ccb0c
Kuby11 <saab112468@gmail.com>
2026-03-17 00:29:40 +0400

    Update game assets and styles; replace JavaScript and CSS files, modify game result messages, and change background color.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.scss';
+import './libs/finisher-header.es5.min'
 
 const choices = ["rock", "paper","scissors"];
 const playerDisplay = document.querySelector(".player-choice") as HTMLElement;
@@ -6,6 +7,7 @@ const computerDisplay = document.querySelector(".computer-choice") as HTMLElemen
 const resultDisplay = document.querySelector(".result") as HTMLElement;
 const playerScoreDisplay = document.querySelector(".player-score") as HTMLElement;
 const computerScoreDisplay = document.querySelector(".computer-score") as HTMLElement;
+
 let playerScore: number = 0;
 let computerScore: number = 0;
 
@@ -18,16 +20,17 @@ function playGame(playerChoice: string){
     result = "IT`S A TIE";
     console.log(result)
   }
+
   else{
     switch(playerChoice){
       case 'rock':
-        result = (computerChoice === 'scissors') ? 'YOU WIN' : 'YOU LOSE'
+        result = (computerChoice === 'scissors') ? 'YOU WON' : 'YOU LOST'
         break;
       case 'paper':
-        result = (computerChoice === 'rock') ? 'YOU WIN' : 'YOU LOSE'
+        result = (computerChoice === 'rock') ? 'YOU WON' : 'YOU LOST'
         break;
       case 'scissors':
-        result = (computerChoice === 'paper') ? 'YOU WIN' : 'YOU LOSE'
+        result = (computerChoice === 'paper') ? 'YOU WON' : 'YOU LOST'
         break;
     }
   }
@@ -37,15 +40,15 @@ function playGame(playerChoice: string){
 
   resultDisplay.classList.remove('win-color', 'lose-color')
   switch(result){
-    case 'YOU WIN':
+    case 'YOU WON':
       resultDisplay.classList.add('win-color')
-      resultDisplay.textContent = "YOU WIN!"
+      resultDisplay.textContent = "YOU WON!"
       playerScore++
       playerScoreDisplay.textContent = playerScore as unknown as string
       break
-    case 'YOU LOSE':
+    case 'YOU LOST':
       resultDisplay.classList.add('lose-color')
-      resultDisplay.textContent = "YOU LOSE!"
+      resultDisplay.textContent = "YOU LOST!"
       computerScore++
       computerScoreDisplay.textContent = computerScore as unknown as string
       break
@@ -53,6 +56,7 @@ function playGame(playerChoice: string){
     resultDisplay.textContent = 'IT`S A TIE'
   }
 }
+
 const rockBtn = document.getElementById('button-rock') as HTMLElement;
 const paperBtn = document.getElementById('button-paper') as HTMLElement;
 const scissorsBtn = document.getElementById('button-scissors') as HTMLElement;
@@ -60,9 +64,11 @@ const scissorsBtn = document.getElementById('button-scissors') as HTMLElement;
 rockBtn.addEventListener('click', ()=>{
   playGame('rock')
 })
+
 paperBtn.addEventListener('click', ()=>{
   playGame('paper')
 })
+
 scissorsBtn.addEventListener('click', ()=>{
   playGame('scissors')
 })
